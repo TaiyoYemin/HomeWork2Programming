eb136f12b8e68ab505e2fc4dfeb58cdaf8e68b93
Amir <amir-cohen@campus.technion.ac.il>
2024-07-06 10:47:56 +0300

    Initial commit, just opened a project

---
+++ HW_2/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            System.out.println("Test 1 starts");
+            test1();
+            System.out.println("Test 1 done");
+            System.out.println("--------------------------------------------");
+        } catch (Exception e) {
+            System.out.println("exception " + e);
+        }
+    }
+    public static void test1() {
+        Library library = new Library("Technion Library");
+        library.addBook("Harry Potter and the Sorcerer's Stone", Genre.FANTASY, "J.K. Rowling", "British author, best known for the Harry Potter series.");
+        library.printBooks();
+    }
+}
